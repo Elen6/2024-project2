ae1bc4c73e04eebc35aa468e6ff843c9ab736f8c
Elen6 <elenstepanyan@mail.ru>
2024-01-21 00:22:01 +0400

    some added codes

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function sendEmail() {
+    let email = document.querySelector("#email")
+
+    let password = document.querySelector("#password")
+    alert(email.value + " " + password.value)
+    
+}
+
+
